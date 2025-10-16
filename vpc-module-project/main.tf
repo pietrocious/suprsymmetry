7affd585f5ccfeeffd31384cc1731e5a00b04aea
pietrocious <pietrouni@gmail.com>
2025-10-16 16:08:20 +0000

    Update all regions from eu-west-3 to us-east-1

--- vpc-module-project/main.tf
+++ vpc-module-project/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-3"
+  region = "us-east-1"
 }
 
 # Call the VPC module
@@ -17,7 +17,7 @@ module "dev_vpc" {
   
   vpc_cidr               = "10.0.0.0/16"
   environment            = "dev"
-  availability_zones     = ["eu-west-3a", "eu-west-3b"]
+  availability_zones     = ["us-east-1a", "us-east-1b"]
   public_subnet_cidrs    = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs   = ["10.0.11.0/24", "10.0.12.0/24"]
   enable_nat_gateway     = true
--- vpc-networking/main.tf
+++ vpc-networking/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-3"
+  region = "us-east-1"
 }
 
 # VPC
@@ -35,7 +35,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-west-3a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   
   tags = {
@@ -47,7 +47,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-west-3b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   
   tags = {
@@ -59,7 +59,7 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.11.0/24"
-  availability_zone = "eu-west-3a"
+  availability_zone = "us-east-1a"
   
   tags = {
     Name = "private-subnet-1"
@@ -70,7 +70,7 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.12.0/24"
-  availability_zone = "eu-west-3b"
+  availability_zone = "us-east-1b"
   
   tags = {
     Name = "private-subnet-2"
